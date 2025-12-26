1970fe3154e29b7b28a9d921cffd5f3a036dee80
HARRY-hussein <rahmatovhusanjon@icloud.com>
2025-12-26 21:17:13 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ console.log("Web Serverni boshlanishi");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+const { error } = require("console");
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1 ->Expressga kirib kelayotgan ma'lumotga bog'liq kodlar
 app.use(express.static("public")); //clientlarga public folderni ochiq qilib beradi. CSS shuni ichiga joylashadi
@@ -15,8 +26,12 @@ app.set("view engine", "ejs"); //EJS - Backendda HTML frontendni yasaymiz
 
 // 4 -> Routing kodlari
 app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({ test: "success" });
+  console.log(req.body);
+  res.json({ test: "success" });
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 
 app.get("/", function (req, res) {
