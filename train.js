576716e8860f3e2703dda5c31f841a4380b7f04e
HARRY-hussein <rahmatovhusanjon@icloud.com>
2026-01-01 12:57:46 +0900

    Feat: modified: train.js

--- train.js
+++ train.js
@@ -1,9 +1,25 @@
 // ------------- B TASK -------------
+/* Shart:
+Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
+MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
+*/
+function countDigits(string) {
+  let count = 0;
+  for (let char of string) {
+    if (char >= 0 && char <= 9) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(countDigits("hfwu2nx92bd74jcs9"));
 
 // ------------- TASK A -------------
-// Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni
-// ikkinchi parametrdagi so'zdan qatnashgan sonini return qilishi kerak boladi.
-
+/* Shart:
+Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni
+ikkinchi parametrdagi so'zdan qatnashgan sonini return qilishi kerak boladi.
+*/
+/*
 function countLetters(letter, text, callback) {
   if (typeof letter !== "string" || typeof text !== "string") {
     callback("Insert string only", null);
@@ -21,3 +37,4 @@ countLetters("s", "Mississippi", (err, data) => {
   if (err) console.log("ERROR:", err);
   else console.log("result:", data);
 });
+*/
